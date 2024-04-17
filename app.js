2aa7f20575eff8d6b55c05d17139d47c0618c211
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-17 14:32:32 +0200

    [Working] index.php communicates correctly with my script

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+
 const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            //Stringa per la chiamata API
+            myUrl: "dischi.json",
+            //Array vuoto dove inserire i miei file 
+            disks: [],
         }
+    },
+
+    mounted() {
+        //Faccio la mia chiama ajax con axios (importato con cdn)
+        axios.get(this.myUrl)
+            .then((res) => {
+                //Vedo cosa ho
+                console.log(res);
+                //Aggiundo all'array
+                this.disks = res.data;
+            }).catch((error)=>{
+                console.log(error.message);
+            })
     }
 }).mount('#app')
--- index.php
+++ index.php
@@ -2,18 +2,36 @@
 
 <!doctype html>
 <html lang='en'>
+
 <head>
-<meta charset='utf-8'>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<title>PHP - Dischi</title>
-<link href='./style.css' rel='stylesheet'>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>PHP - Dischi</title>
+    <link href='./style.css' rel='stylesheet'>
 </head>
+
 <body>
-<div id='app'>
-<h1>Hello Dischi</h1>
-</div>
-<!-- Development only cdn, disable in production -->
-<script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
-<script src='./app.js'></script>
+    <div id='app'>
+        <!-- Verifico il corretto collegamento-->
+        <ul>
+            <li v-for="disk in disks">
+                {{disk.title}}
+            </li>
+        </ul>
+    </div>
+
+
+
+
+
+    <!-- Vue -->
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js"
+        integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- My js -->
+    <script src='./app.js'></script>
 </body>
+
 </html>
