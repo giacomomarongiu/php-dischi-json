8f02322f4bf3359717be83855227514a2de7cf61
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-17 14:15:28 +0200

    [Init] First Structure added

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<!doctype html>
+<html lang='en'>
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title>PHP - Dischi</title>
+<link href='./style.css' rel='stylesheet'>
+</head>
+<body>
+<div id='app'>
+<h1>Hello Dischi</h1>
+</div>
+<!-- Development only cdn, disable in production -->
+<script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+<script src='./app.js'></script>
+</body>
+</html>
